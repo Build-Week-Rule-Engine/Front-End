b3d10455bf21356f37a469c9ff67a108062870da
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-04 16:18:50 -0800

    added reducer, actions files and protected route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import './App.css';
 import Dashboard from "./engine/Dashboard";
+
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import ProtectedRoute from './ProtectedRoute';
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import { reducer } from "./reducers";
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Dashboard /> */}
+        <ProtectedRoute path="/dashboard" component={Dashboard} />
+      </div>
+    </Router>
   );
 }
 
---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,188 @@
+import {
+    FETCHING_USER_START,
+    FETCHING_USER_SUCCESS,
+    FETCHING_USER_FAILURE,
+
+    ADDING_USER_START,
+    ADDING_USER_SUCCESS,
+    ADDING_USER_FAILURE,
+
+    FETCHING_FORM_START,
+    FETCHING_FORM_SUCCESS,
+    FETCHING_FORM_FAILURE,
+
+    ADDING_FORM_START,
+    ADDING_FORM_SUCCESS,
+    ADDING_FORM_FAILURE,
+
+    EDITING_FORM_START,
+    EDITING_FORM_SUCCESS,
+    EDITING_FORM_FAILURE,
+
+    DELETING_FORM_START,
+    DELETING_FORM_SUCCESS,
+    DELETING_FORM_FAILURE,
+
+    FETCHING_TREE_START,
+    FETCHING_TREE_SUCCESS,
+    FETCHING_TREE_FAILURE,
+
+    ADDING_TREE_START,
+    ADDING_TREE_SUCCESS,
+    ADDING_TREE_FAILURE,
+
+    EDITING_TREE_START,
+    EDITING_TREE_SUCCESS,
+    EDITING_TREE_FAILURE,
+
+    DELETING_TREE_START,
+    DELETING_TREE_SUCCESS,
+    DELETING_TREE_FAILURE,
+} from "../actions";
+
+const initialState = {
+
+};
+
+const testState = {
+
+}
+
+export const reducer = (state = initialState, action) => {
+    
+    switch (action.type) {
+    case FETCHING_USER_START:
+
+        return;
+
+    case FETCHING_USER_SUCCESS:
+
+        return;
+
+    case FETCHING_USER_FAILURE:
+
+        return;
+
+
+    case ADDING_USER_START:
+
+        return;
+
+    case ADDING_USER_SUCCESS:
+
+        return;
+
+    case ADDING_USER_FAILURE:
+
+        return;
+
+
+    case FETCHING_FORM_START:
+
+        return;
+
+    case FETCHING_FORM_SUCCESS:
+
+        return;
+
+    case FETCHING_FORM_FAILURE:
+
+        return;
+
+
+    case ADDING_FORM_START:
+
+        return;
+
+    case ADDING_FORM_SUCCESS:
+
+        return;
+
+    case ADDING_FORM_FAILURE:
+
+
+        return;
+
+    case EDITING_FORM_START:
+
+        return;
+
+    case EDITING_FORM_SUCCESS:
+
+        return;
+
+    case EDITING_FORM_FAILURE:
+
+        return;
+
+
+    case DELETING_FORM_START:
+
+        return;
+
+    case DELETING_FORM_SUCCESS:
+
+        return;
+
+    case DELETING_FORM_FAILURE:
+
+        return;
+
+
+    case FETCHING_TREE_START:
+
+        return;
+
+    case FETCHING_TREE_SUCCESS:
+
+        return;
+
+    case FETCHING_TREE_FAILURE:
+
+        return;
+
+
+    case ADDING_TREE_START:
+
+        return;
+
+    case ADDING_TREE_SUCCESS:
+
+        return;
+
+    case ADDING_TREE_FAILURE:
+
+        return;
+
+
+    case EDITING_TREE_START:
+
+        return;
+
+    case EDITING_TREE_SUCCESS:
+
+        return;
+
+    case EDITING_TREE_FAILURE:
+
+        return;
+
+
+    case DELETING_TREE_START:
+
+        return;
+
+    case DELETING_TREE_SUCCESS:
+
+        return;
+
+    case DELETING_TREE_FAILURE:
+
+        return;
+
+    default:
+
+        return state;
+
+    }
+}
