125da3699276c6348fcba5d1ed84cf6a86df518f
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-04 23:45:41 -0800

    updated initialState to contain only the initial form and tree objects without subproperties

--- my-app/src/reducers/initialState.js
+++ my-app/src/reducers/initialState.js
@@ -4,15 +4,14 @@ import { initialTrees } from "./initialTrees";
 const initialState = {
 
     formsAvailable: initialForms,
-    formId: initialForms[0].formId,
-    formName: initialForms[0].name,
+    form: initialForms[0],
 
     treesAvailable: initialTrees,
-    treeId: initialTrees[0].treeId,
-    treeName: initialTrees[0].name,
-    tree: initialTrees[0].tree,
+    tree: initialTrees[0],
+    
+    lastSelectedOption = "",
 
-    errorMessage: state.errorMessage,
+    errorMessage: "",
 
 };
 
