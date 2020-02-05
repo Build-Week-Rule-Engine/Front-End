3aa5f155c401b6bc3d894b48ee0d2e7ddfce9d9d
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-02-05 21:19:03 +0100

    got started on the FormRequests page

---
+++ my-app/src/Account/PendingRequests/PendingRequests.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+const PendingRequests = (props) => {
+
+  const [forms, setForms] = useState([])
+  const [filterForms, setFilterForms]
+}
+
+export default PendingRequests
