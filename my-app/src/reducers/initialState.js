9b98fd1f9bb2e98a0cf705ca5d7f446f0c95ab31
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-07 06:58:21 -0800

    stopped loading form dummy data in initialState

--- my-app/src/reducers/initialState.js
+++ my-app/src/reducers/initialState.js
@@ -3,8 +3,10 @@ import { initialTrees } from "./initialTrees";
 
 const initialState = {
 
-    formsAvailable: initialForms,
-    form: initialForms[0],
+    // formsAvailable: initialForms,
+    // form: [initialForms[0]],
+    formsAvailable: [],
+    form: undefined,
 
     treesAvailable: initialTrees,
     tree: initialTrees[0],
